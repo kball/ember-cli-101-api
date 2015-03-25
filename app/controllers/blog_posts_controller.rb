bb2f1f1208e121f6c0e3e76948eba66b51724f2c
Kevin Ball <kmball11@gmail.com>
2015-03-24 19:44:10 -0700

    Add creation action

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,4 +8,14 @@ class BlogPostsController < ApplicationController
     @blog_post = BlogPost.find(params[:id])
     render :json => @blog_post
   end
+
+  def create
+    @blog_post = BlogPost.create(post_params)
+    render :json => @blog_post
+  end
+
+  private
+    def post_params
+      params.require(:blog_post).permit(:title,:body,:published_date)
+    end
 end
